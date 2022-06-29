1681ab7ceaf55eb779a0509b434bb0b43b3abf31
Ankur Raj <64456355+ancoreraj@users.noreply.github.com>
2022-06-29 22:00:47 +0530

    Queue

---
+++ queue.js
@@ -0,0 +1,46 @@
+class Queue {
+    constructor() {
+        this.elements = {};
+        this.head = 0;
+        this.tail = 0;
+    }
+    push(element) {
+        this.elements[this.tail] = element;
+        this.tail++;
+    }
+    pop() {
+        const item = this.elements[this.head];
+        delete this.elements[this.head];
+        this.head++;
+        // return item;
+    }
+    front() {
+        return this.elements[this.head];
+    }
+    length() {
+        return this.tail - this.head;
+    }
+    empty() {
+        if(this.length() == 0) return true;
+
+        return false;
+    }
+}
+
+
+let q = new Queue();
+
+for (let i = 1; i <= 7; i++) {
+    q.push(i);
+}
+
+console.log(q.front()); // 1
+console.log(q.length()); // 7
+q.pop();
+console.log(q.front()) // 2
+console.log(q.length()); // 6
+
+while(!q.empty()){
+    console.log(q.front());
+    q.pop();
+}
